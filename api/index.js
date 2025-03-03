8c573542dd62e5d7498f6d9b39166845972dc57d
Jerko Suodenjoki <jerko.suodenjoki@gmail.com>
2025-03-03 13:58:31 +0200

    muutokset

--- api/index.js
+++ api/index.js
@@ -660,6 +660,14 @@ app.get("/api/clear-memory", (req, res) => {
 
   console.log("✅ Kaikki muisti tyhjennetty!");
 
+  // Aseta Cache-Control-otsikko estämään välimuistitus
+  res.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+
   res.json({
     success: true,
     message: "Kaikki muisti tyhjennetty",
